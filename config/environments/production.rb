18fdb1076dbd68d057c7f03c417b2d49e2c64420
Tobias Kraze <tobias@kraze.eu>
2014-06-16 10:47:29 +0200

    disable emails for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Gitlab::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 end
